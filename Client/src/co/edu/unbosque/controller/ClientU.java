67e56d797ba0b052aa51be9295b0abc4486c84d2
josed <josed@LAPTOP-VDJ7P4DH>
2023-08-22 14:15:49 -0500

    Version 1.0.0: This update introduces significant changes to the ClientU class. These new changes entail the ability to receive messages from and send messages to the server. Additionally, the option to disconnect from the server by typing the word "Salir" has been incorporated. These changes also successfully establish a connection between the server and the client, both locally and across different machines.
    A substantial restructuring of the 'run' method has been implemented,
    retaining only the 'while' loop but with a different condition.

--- Client/src/co/edu/unbosque/controller/ClientU.java
+++ Client/src/co/edu/unbosque/controller/ClientU.java
@@ -32,30 +32,39 @@ public class ClientU extends Thread {
 	}
 
 	public void run() {
-		
+
 		String line = "";
-		
+
 		try {
-			 
-			
-            while (sn.hasNext()) {
-            	this.socket = new Socket(this.address, this.port); 
-                System.out.println("Connected"); 
-				this.out = new DataOutputStream(socket.getOutputStream()); 
-                line=sn.nextLine();
-                this.out.writeUTF(line);
-                 
+
+			while (!line.equalsIgnoreCase("Salir")) {
+				this.socket = new Socket(address, port);
+				System.out.println("Cliente: ");
+				line = sn.nextLine();
+
+				out = new DataOutputStream(socket.getOutputStream());
+				out.writeUTF(line);
+				out.flush();
+
+				if (line.equalsIgnoreCase("Salir")) {
+					break;
+				}
+
+				in = new DataInputStream(socket.getInputStream());
+				String answerServer = in.readUTF();
+				System.out.println("Servidor: " + answerServer);
+				socket.close();
 			}
+			System.out.println("Se desconecto del servidor");
 		} catch (IOException i) {
 			System.out.println(i);
 		}
-        }
-	
+	}
 	
 	 public static void main(String args[]) 
 	    { 
 	    	//Servidor server = new Servidor(5001); 
-	    	ClientU client = new ClientU("127.0.0.1", 8000); 
+	    	ClientU client = new ClientU("192.168.10.6", 8000); 
 	    	client.start();
 	    	
 	    } 
